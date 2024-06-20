25a2d26270ae8bc527300561413f560adae65a8b
Stefan <stefankhor2002@gmail.com>
2024-06-20 15:55:27 +0800

    Add SplashScreen with explicit intent

--- app/src/main/java/com/y_social_media_app/SplashScreen.java
+++ app/src/main/java/com/y_social_media_app/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.y_social_media_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,18 +10,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashScreen extends AppCompatActivity {
 
-
+    FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth != null) {
+            currentUser = mAuth.getCurrentUser();
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser user = mAuth.getCurrentUser();
+                if (user == null) {
+                    Intent loginIntent = new Intent(SplashScreen.this, LoginActivity.class);
+                    startActivity(loginIntent);
+                    finish();
+                } else {
+                    Intent dashboardIntent = new Intent(SplashScreen.this, DashboardActivity.class);
+                    dashboardIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(dashboardIntent);
+                    finish();
+                }
+            }
+        }, 1000);
     }
 }
