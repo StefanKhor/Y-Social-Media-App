88d54c864fa6057322eccd9cc8fffa958821623a
Stefan <stefankhor2002@gmail.com>
2024-06-20 17:40:09 +0800

    Modify Login Activity and XML

--- app/src/main/java/com/y_social_media_app/LoginActivity.java
+++ app/src/main/java/com/y_social_media_app/LoginActivity.java
@@ -8,17 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.y_social_media_app.databinding.ActivityLoginBinding;
+import com.y_social_media_app.databinding.ActivityMainBinding;
+
 public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        
     }
 }
