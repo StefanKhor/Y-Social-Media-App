aa5957fce8ede6c8922fd7e6ef6c7ec035c146d0
Stefan <stefankhor2002@gmail.com>
2024-06-20 17:39:00 +0800

    Add register Layout

--- app/src/main/java/com/y_social_media_app/RegisterActivity.java
+++ app/src/main/java/com/y_social_media_app/RegisterActivity.java
@@ -9,7 +9,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
