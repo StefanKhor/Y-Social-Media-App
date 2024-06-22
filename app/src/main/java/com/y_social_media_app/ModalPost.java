88eb54047a815e23e98ab9320ab510fa67654ed4
Stefan <stefankhor2002@gmail.com>
2024-06-22 14:27:58 +0800

    Refactor code structure

---
+++ app/src/main/java/com/y_social_media_app/ModalPost.java
@@ -0,0 +1,13 @@
+//package com.y_social_media_app;
+//
+//public class ModalPost {
+//    public ModalPost(){
+//
+//    }
+//
+//    String description;
+//
+//    public String getDescription() {
+//
+//    }
+//}
