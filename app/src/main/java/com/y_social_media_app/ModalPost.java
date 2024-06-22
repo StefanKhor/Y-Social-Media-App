a6637f77072e4593d3932f31c267f564ace50840
Stefan <stefankhor2002@gmail.com>
2024-06-23 00:21:16 +0800

    Add temporary ModalPost

--- app/src/main/java/com/y_social_media_app/ModalPost.java
+++ app/src/main/java/com/y_social_media_app/ModalPost.java
@@ -1,13 +1,62 @@
-//package com.y_social_media_app;
-//
-//public class ModalPost {
-//    public ModalPost(){
-//
-//    }
-//
-//    String description;
-//
-//    public String getDescription() {
-//
-//    }
-//}
+package com.y_social_media_app;
+
+public class ModalPost {
+    public ModalPost(){
+    }
+
+    public  ModalPost(String title, String description, String timestamp){
+        this.title = title;
+        this.description = description;
+        this.timestamp = timestamp;
+    }
+
+    String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    String timestamp;
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    String authorImage;
+
+    public String getAuthorImage() {
+        return authorImage;
+    }
+
+    public void setAuthorImage(String authorImage) {
+        this.authorImage = authorImage;
+    }
+
+    String postImage;
+
+    public String getPostImage() {
+        return postImage;
+    }
+
+    public void setPostImage(String postImage) {
+        this.postImage = postImage;
+    }
+}
