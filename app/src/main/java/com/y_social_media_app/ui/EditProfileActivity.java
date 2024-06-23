0efb2bd9b7eebf809627a761ab2f65409f109fe1
JJ <jiajianmmu@gmail.com>
2024-06-23 17:14:58 +0800

    Feature/Upload profile image and cover image.

--- app/src/main/java/com/y_social_media_app/ui/EditProfileActivity.java
+++ app/src/main/java/com/y_social_media_app/ui/EditProfileActivity.java
@@ -20,7 +20,6 @@ import androidx.annotation.Nullable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
